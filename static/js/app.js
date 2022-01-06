7071196ada634565f684ad135e374d25eab2557c
Tozerh <tozer.hammond@gmail.com>
2022-01-06 07:35:43 -0500

    Commit for mod11 - 11.6.2

--- static/js/app.js
+++ static/js/app.js
@@ -9,22 +9,20 @@ function buildTable(data) {
     tbody.html(""); 
 
 
-}; 
-
 // loops through each object of data and append a row 
 // and cells for each value in a given row
 data.forEach((dataRow) => {
 
-let row = tbody.append("tr"); //using let so that the var row is not available outside of the forEach fnc
+    let row = tbody.append("tr"); //using let so that the var row is not available outside of the forEach fnc
 
 // loops through each field in dataRow and adds each value as a table cell "(td)"
-Object.values(dataRow).forEach((val) => {
-    let cell = row.append("td"); 
-    cell.text(val)
-    }
+    Object.values(dataRow).forEach((val) => {
+        let cell = row.append("td"); 
+        cell.text(val);
+        }
     ); 
 });
-
+}
 function handleClick() {
     let date = d3.select("#datetime").property("value");
     let filteredData = tableData; 
@@ -33,11 +31,13 @@ function handleClick() {
 
 
 // "===" = exact match for value and type
-if (date) {filteredData = filteredData.filter(row => row.datetime === date);};
+    if (date) {
+        filteredData = filteredData.filter(row => row.datetime === date);
+    };
 // redbuilds table with the filteredData -- if no date entered, then filteredData 
 // will be the original tableData
-buildTable(filteredData);
-}
+    buildTable(filteredData);
+};
 
 // listens for "click"; when button is clicked, triggers on, which runs func. handleClick
 d3.selectAll("#filter-btn").on("click", handleClick);
