e78d4392660c438008d4eff6e67e36edfec30ae1
Tozerh <tozer.hammond@gmail.com>
2022-01-05 19:45:54 -0500

    push to commit after creating handleClick function mod11 - 11.5.4

--- static/js/app.js
+++ static/js/app.js
@@ -24,3 +24,23 @@ Object.values(dataRow).forEach((val) => {
     }
     ); 
 });
+
+function handleClick() {
+    let date = d3.select("#datetime").property("value");
+    let filteredData = tableData; 
+// pseudocode practice: "if (a date is entered) {Filter the 
+// default data to show only the date entered};""
+
+
+// "===" = exact match for value and type
+if (date) {filteredData = filteredData.filter(row => row.datetime === date);};
+// redbuilds table with the filteredData -- if no date entered, then filteredData 
+// will be the original tableData
+buildTable(filteredData);
+}
+
+// listens for "click"; when button is clicked, triggers on, which runs func. handleClick
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+// creates basic table filled with unfiltered data from our data array, "data.js"
+buildTable(tableData);
